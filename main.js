7df454520412dbb2c1fb1592ffe0621328de4df5
John Francis <johnmosima7@gmail.com>
2024-07-31 17:39:33 +0300

    add vite config

--- main.js
+++ main.js
@@ -1,84 +1,248 @@
 const toggle = document.getElementById("menu-toggle")
 const links = document.getElementById("menu-links")
 const cards = document.getElementById("cards")
+let isLike;
 document.querySelector('video').playbackRate = 0.5;
+// const db = firebase.firestore()
+// const increment = firebase.firestore.FieldValue.increment(1)
+// const decrement = firebasse.firestore.FieldValue.increment(-1)
 
-const cardData = [{
-    author: "Nestor Renja",
-    title: "Boost Online Presence with Unik Digital Marketing's Services",
-    description: "In the ever-evolving landscape of digital marketing, staying ahead of the competition is crucial for businesses looking to thrive in the...",
-    viewCount: 0,
-    likeCount: 0,
-    img: "./media/001.jpg"
-}, {
-    author: "Nestor Renja",
-    title: "Elevate Your Brand with Unik Identity by Unik Digital",
-    description: "In today's fast-paced digital world, establishing a strong online presence is essential for businesses to thrive. This is where Unik...",
-    viewCount: 2,
-    likeCount: 1,
-    img: "./media/002.jpg"
-},, {
-    author: "John Francis",
-    title: "UNIK AI : An Intelligent Digital Marketing Tool",
-    description: "Imagine an AI that recommends a digital marketing strategy based on your current business needs and siz...",
-    viewCount: 0,
-    likeCount: 0,
-    img: "./media/004.jpg"
-}, {
-    author: "Nestor Renja",
-    title: "Unlock Your Business Potential with Unik Digital Marketing",
-    description: "In the era of digital dominance, having a strong online presence is no longer an option but a necessity for businesses. Unik Digital...",
-    viewCount: 1,
-    likeCount: 1,
-    img: "./media/003.jpg"
-}]
+
+
 
 toggle.addEventListener("click", () => {
     // show nav links
+    console.log("nav unleashed")
     links.classList.value.split(" ")[1] == "visible" ? links.classList.remove("visible") : links.classList.add("visible");
 
-
 })
 
-cardData.forEach((card) => {
-    const cardi = document.createElement("div");
-    cardi.classList.add("blurbs-card")
+const scrollAnimObjects = document.querySelectorAll(".animReady")
 
-    const cardImage = document.createElement("div");
-    cardImage.classList.add("article-card");
-    // Set the background image
-    cardImage.style.backgroundImage = `url(${card.img})`;
-    cardi.appendChild(cardImage);
-
-    const cardDesc = document.createElement("div");
-    cardDesc.classList.add("desc-card")
-    cardDesc.innerHTML = `<article class="article">
-            <div class="articlee" style="display:flex;">
-                <div class="author"><span title="${card.author}" >By ${card.author}</span></div>
-                <div class="main">
-                    <a class="main-link" href="#"><p class="article-header" style="-webkit-line-clamp:3;">${card.title}</p></a>
-                    <div class="main-desc"><p style="-webkit-line-clamp:3">${card.description}</p></div>
-                </div>
-                <div class="likes">
-                    <span class="line"></span>
-                    <div class="likes-container">
-                        <div class="views-span"><span class="P7Wc3k has-custom-focus" tabindex="0">${card.viewCount} views</span></div>
-                        <div class="likes-span">
-                            <span class="h7K_lu" data-hook="like-button-with-count__like-count">
-                                <span aria-hidden="true" class="like-count">${card.likeCount}</span>
-                            </span>
-                            <span class="likes-svg">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="19" viewBox="0 0 19 19" role="img"><path d="M9.44985848,15.5291774 C9.43911371,15.5362849 9.42782916,15.5449227 9.41715267,15.5553324 L9.44985848,15.5291774 Z M9.44985848,15.5291774 L9.49370677,15.4941118 C9.15422701,15.7147757 10.2318883,15.0314406 10.7297038,14.6971183 C11.5633567,14.1372547 12.3827081,13.5410755 13.1475707,12.9201001 C14.3829188,11.9171478 15.3570936,10.9445466 15.9707237,10.0482572 C16.0768097,9.89330422 16.1713564,9.74160032 16.2509104,9.59910798 C17.0201658,8.17755699 17.2088969,6.78363112 16.7499013,5.65913129 C16.4604017,4.81092573 15.7231445,4.11008901 14.7401472,3.70936139 C13.1379564,3.11266008 11.0475663,3.84092251 9.89976068,5.36430396 L9.50799408,5.8842613 L9.10670536,5.37161711 C7.94954806,3.89335486 6.00516066,3.14638251 4.31830373,3.71958508 C3.36517186,4.00646284 2.65439601,4.72068063 2.23964629,5.77358234 C1.79050315,6.87166888 1.98214559,8.26476279 2.74015555,9.58185512 C2.94777753,9.93163559 3.23221417,10.3090129 3.5869453,10.7089994 C4.17752179,11.3749196 4.94653811,12.0862394 5.85617417,12.8273544 C7.11233096,13.8507929 9.65858244,15.6292133 9.58280954,15.555334 C9.53938013,15.5129899 9.48608859,15.5 9.50042471,15.5 C9.5105974,15.5 9.48275828,15.5074148 9.44985848,15.5291774 Z"></path></svg>
-                            </span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </article>
-    `;
-    cardi.appendChild(cardDesc);
-
-    cards.appendChild(cardi)
-    
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove("animReady");
+            entry.target.classList.add("now-visible");
+            observer.unobserve(entry.target);
+        }
+    })
+}, {
+    threshold: 0.5
 })
+
+scrollAnimObjects.forEach((animObject) => {
+    observer.observe(animObject)
+})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    function startLoader() {
+        let counterElement = document.querySelector(".count p");
+        let currentValue = 0;
+
+        function updateCounter() {
+            if (currentValue < 100) {
+                let increment = Math.floor(Math.random() * 10) + 1;
+                currentValue = Math.min(currentValue + increment, 100);
+                counterElement.textContent = currentValue;
+
+                let delay = Math.floor(Math.random() * 200) + 25;
+                setTimeout(updateCounter, delay);
+            }
+        }
+        updateCounter();
+    }
+
+    startLoader();
+    gsap.to(".count", {
+        opacity: 0,
+        delay: 3.5,
+        duration: 0.5
+    });
+
+    let textWrapper = document.querySelector(".ali6");
+    textWrapper.innerHTML = textWrapper.textContent.replace(/./g, "<span class='letter'>$&</span>");
+
+    anime.timeline({
+            loop: false
+        })
+        .add({
+            targets: ".ali6 .letter",
+            translateY: [-100, 0],
+            easing: "easeOutExpo",
+            duration: 1500,
+            delay: (el, i) => 30 * i,
+            opacity: [0, 1]
+        })
+        .add({
+            targets: '.ali6 .letter',
+            translateY: [0, 100],
+            easing: "easeOutExpo",
+            duration: 3000,
+            delay: (el, i) => 2000 + 30 * i,
+            opacity: [1, 0]
+        });
+
+    gsap.to(".pre-loader", {
+        scale: 0.5,
+        ease: "power4.inOut",
+        duration: 2,
+        delay: 3
+    })
+
+    gsap.to(".loader", {
+        height: "0",
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 3.75
+    })
+
+    gsap.to(".loader-bg", {
+        height: "0",
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 4
+    })
+
+    gsap.to(".loader-2", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 3.5
+    })
+
+    // gsap.to(".load-header h1", {
+    //     y: 200,
+    //     ease: "power4.inOut",
+    //     duration: 1.5,
+    //     delay: 3.75,
+    //     stagger: 0.05
+    // })
+    gsap.from(".hero-header h1", {
+        opacity: 1,
+        y: 200,
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 3.75
+    })
+    gsap.from(".hero-desc p", {
+        opacity: 1,
+        y: 200,
+        ease: "power4.inOut",
+        duration: 1.5,
+        delay: 4
+    })
+    gsap.from([".nav-logo", ".menu-item", ".search", ".menu"], {
+        opacity: 0,
+        y: -100,
+        transformOrigin: 'center',
+        duration: 2,
+        stagger: 0.05,
+        ease: 'power4.out',
+        delay: 4
+    })
+    gsap.from(".hero-img", {
+        scrollTrigger: {
+            trigger: ".hero-img",
+            end: 'bottom 20%',
+        },
+        scale: .8,
+        opacity: 0,
+        y: 200,
+        transformOrigin: 'center',
+        duration: 2,
+        ease: 'power4.out'
+    })
+
+    gsap.from(".about-pictures", {
+        scrollTrigger: {
+            trigger: ".about-pictures",
+            end: 'bottom 20%',
+        },
+        scale: .8,
+        opacity: 0,
+        transformOrigin: 'center',
+        duration: 2,
+        ease: 'power4.out'
+    })
+
+    gsap.from([".about-header", ".about-sub", ".about-desc"], {
+        scrollTrigger: {
+            trigger: ".about-header",
+            end: 'bottom 20%',
+        },
+        opacity: 0,
+        x: 200,
+        transformOrigin: 'center',
+        duration: 2,
+        stagger: 0.25,
+        ease: 'power4.out'
+    })
+
+    gsap.from([".gradient", ".blurbss"], {
+        scrollTrigger: {
+            trigger: ".latest-news",
+            end: 'bottom 20%',
+        },
+        opacity: 0,
+        scale: .8,
+        transformOrigin: 'center',
+        duration: 2,
+        stagger: 0.25,
+        ease: 'power4.out'
+    })
+
+    gsap.from(".insta-title h2", {
+        scrollTrigger: {
+            trigger: ".insta-widget",
+            end: 'bottom 20%',
+        },
+        y: 200,
+        opacity: 0,
+        transformOrigin: 'center',
+        duration: 2,
+        ease: 'power4.out'
+    })
+
+
+    gsap.from(".gallery-items", {
+        scrollTrigger: {
+            trigger: ".gallery-items",
+            end: 'bottom 20%',
+        },
+        opacity: 0,
+        scale: .8,
+        y: 100,
+        transformOrigin: 'center',
+        duration: 2,
+        stagger: 0.25,
+        ease: 'power4.out'
+    })
+
+    gsap.from([".notion", ".canva-design", ".waiting-list-btn"], {
+        scrollTrigger: {
+            trigger: ".notion-container",
+            end: 'bottom 20%',
+        },
+        opacity: 0,
+        scale: .7,
+        transformOrigin: 'center',
+        duration: 2,
+        stagger: 0.25,
+        ease: 'power4.out'
+    })
+
+    // gsap.to(".img", {
+    //     clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+    //     ease: "power4.inOut",
+    //     duration: 1.5,
+    //     delay: 4.5,
+    //     stagger: 0.25
+    // })
+    //gsap.to});100%)",
+    //     ease: "power4.inOut",
+    //     duration: 1.5,
+    //     delay: 4.5,
+    //     stagger: 0.25
+    // })
+});
